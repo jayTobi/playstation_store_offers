57267e4e1e090243babd77abefa86d2f9756ba4f
Joerg H <jhummler@gmail.com>
2021-01-18 10:35:41 +0100

    added parsing of Metacritic scores and merged it with offers

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 This is a simple Python Script for scraping offers from Playstation offer websites.
 
 It can be used for further analysis using the pandas.
+## Adjustments of the offers
+To get the latest offers the `URL` and `MAX_PAGE_NUMBER` constants must be changed.
+These can easily be found by accessing the PS Store open the offers section, if any, and copy the URL from your browser.
 
+## Metacritics (www.metacritic.com)
+Metacritics can now be downloaded manually (webpage, e.g. PS4 Games of All Time -> save as HTML) and imported to add the ratings to the offers for filtering.
+
+## Further development
 More examples will be added later.
 
 Known limitations:
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ SHOW_ERRORS = True
 local_cache_file = "offers.json"
 
 
-def get_data_from_web(url: str, max_page_number: int) -> pd.DataFrame:
+def get_ps_offers_from_web(url: str, max_page_number: int) -> pd.DataFrame:
     games = pd.DataFrame()
     errors = []
     for i in range(1, max_page_number + 1):
-        print(f"Scraping page number {i} of {MAX_PAGE_NUMBER}")
+        print(f"Scraping page number {i} of {max_page_number}")
         response = urlopen(url + str(i), data=None)
         soup = BeautifulSoup(response, 'html.parser')
         SECTION_DETAILS = 'class="ems-sdk-product-tile__details"'
@@ -48,8 +48,32 @@ def convert_data_types(df: pd.DataFrame) -> pd.DataFrame:
         "%", "")).astype(float)
     return df
 
+def get_metacritics_from_files(file_name_prefix: str = "Meta", file_name_suffix: str = ".html") -> pd.DataFrame:
+    SUMMARY_CLASS = "clamp-summary-wrap"
+    critics = pd.DataFrame()
+    errors = []
+    for i in range(1, 9 + 1):
+        file = f"{file_name_prefix}{i}{file_name_suffix}"
+        print(f"Scraping file: '{file}'")
+        with open(file) as response:
+            soup = BeautifulSoup(response, 'html.parser')
+            all_summaries = soup.find_all(
+                name="td", class_=SUMMARY_CLASS)
+
+            for td in all_summaries:
+                try:
+                    game_title = td.find(name="h3").text
+                    rating = int(td.find(name="a", class_="metascore_anchor").div.text)
+                    critics = critics.append({"title": game_title, "rating": rating}, ignore_index=True)
+                except Exception as e:
+                    errors.append(f"error {e} on page {i} with element {td}")
+    print(f"fetched {len(critics)} successfully and found {len(errors)} errors.")
+    if SHOW_ERRORS:
+        print(f"Errors {errors}")
+    return critics
+
 #%%
-games = get_data_from_web(URL, MAX_PAGE_NUMBER)
+games = get_ps_offers_from_web(URL, MAX_PAGE_NUMBER)
 games = convert_data_types(games)
 games.to_json(local_cache_file)
 #%%
@@ -61,4 +85,13 @@ print(sorted.head())
 sweet_range = games.loc[(games["reduced_price"] < 30) & (games["reduced_price"] > 9)]
 # %%
 sweet_range = sweet_range.sort_values(by="reduction_percentage", ascending=True)
+# %%
+critics = get_metacritics_from_files(file_name_prefix="meta/Meta")
+critics_file = "all_metacritics_jan2021.json"
+critics.to_json("all_metacritics_jan2021.json")
+print("Metacritics exported as JSON")
+# %%
+offers_with_ratings = games.merge(critics, on="title", how="left")
+# %%
+offers_with_ratings[(offers_with_ratings["rating"] > 80)].head(50).sort_values(by="rating", ascending=False)
 # %%
